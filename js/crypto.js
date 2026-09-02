006eddb3c6366bce8ee4cb246b73a0e086af9b8a
Nazla Azrina <nazlaazrina1812@gmail.com>
2026-09-02 11:13:50 +0700

    Add files via upload

---
+++ js/crypto.js
@@ -0,0 +1,1 @@
+async function sha256(text){const data=new TextEncoder().encode(text);const hash=await crypto.subtle.digest('SHA-256',data);return [...new Uint8Array(hash)].map(b=>b.toString(16).padStart(2,'0')).join('')}
